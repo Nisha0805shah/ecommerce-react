1f44cfb11bdb660f546aa13ac6e3cfb191ed2a8b
Nisha0805shah <nishashah0805@gmail.com>
2023-04-05 17:05:27 +0530

    fix navbar

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <div className='container mb-5'>
+            <div className='container mb-5 my-5'>
                 <div className='row'>
                     <div className='col-12 text-center py-4 my-4'>
                         <h1 className='display-6 fw-bolder text-center'> Have some Questions?</h1>
--- src/component/Product.js
+++ src/component/Product.js
@@ -59,10 +59,10 @@ const Product = () => {
     const ShowProduct = () => {
         return (
             <>
-                <div className='col-md-6'>
+                <div className='col-md-6 my-5'>
                     <img src={product.image} alt={product.title} height={"400px"} width={"400px"} />
                 </div>
-                <div className='col-md-6'>
+                <div className='col-md-6 my-5'>
                     <h4 className='text-uppercase text-black-50'>{product.category}</h4>
                     <h1 className='display-5'>{product.title}</h1>
                     <p className='lead fw-bolder'>Rating {product.rating && product.rating.rate}<i className='fa fa-star'></i></p>
